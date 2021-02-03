a2d2d3d09f020c51d502d62befbac2e2464ca257
fu-jimoto <sfujimoto@vcube.co.jp>
2021-02-03 20:45:15 +0900

    add audience button

--- index.js
+++ index.js
@@ -67,7 +67,8 @@ async function joinRtm() {
 
               const room = $(`
                 <div id="player-wrapper-${memberId}" >
-                  <button id="" type="submit" class="btn btn-primary btn-sm" onclick="join('${information.channelName}','','host')">Room(Host:${memberId})</button>
+                  <button id="" type="submit" class="btn btn-primary btn-sm" onclick="join('${information.channelName}','','host')">Join Speaker(Host:${memberId})</button>
+                  <button id="" type="submit" class="btn btn-primary btn-sm" onclick="join('${information.channelName}','','')">Join Audience(Host:${memberId})</button>
                   <button id="" type="submit" class="btn btn-primary btn-sm" onclick="leave()">Leave</button>
                 </div>
                 <br />
